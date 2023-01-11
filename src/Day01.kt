eb30e5cbbe14bbecf676bdaf4ae5a3fb23f4f9d3
xiaofei27 <xiaofei27@staff.weibo.com>
2023-01-11 16:43:51 +0800

    Complete Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
+import java.util.Spliterator
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val elves = input.split { it.isBlank() }
+        return elves.maxOf {
+            it.sumOf { value -> value.toInt() }
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elves = input.split { it.isBlank() }
+
+        return elves.sortedByDescending { it.sumOf { value -> value.toInt() } }
+            .take(3)
+            .sumOf {
+                it.sumOf { value -> value.toInt() }
+            }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
     println(part1(input))
+
     println(part2(input))
+}
+
+public inline fun <T> Iterable<T>.split(predicate: (T) -> Boolean): List<List<T>> {
+    return fold(mutableListOf<MutableList<T>>()) { lists, current ->
+        if (predicate(current)) {
+            lists.add(mutableListOf())
+        } else {
+            lists.lastOrNull()?.add(current) ?: run {
+                lists.add(mutableListOf<T>().apply {
+                    this + current
+                })
+            }
+        }
+        lists
+    }
 }
